64a0b72291046165efd8bd8be35645783842c6f6
annafkt <44895565+annafkt@users.noreply.github.com>
2024-01-07 15:25:19 +0100

    Add script

---
+++ faq-accordion-card/script.js
@@ -0,0 +1,17 @@
+const questionWrappers = document.querySelectorAll('.q-wrapper');
+
+questionWrappers.forEach((wrapper) => {
+  wrapper.addEventListener('click', (e) => {
+    questionWrappers.forEach((wrapper) => {
+      if (wrapper == e.currentTarget) {
+        wrapper.firstElementChild.classList.toggle('h3-active');
+        wrapper.lastElementChild.classList.toggle('svg-animation');
+        wrapper.nextElementSibling.classList.toggle('p-visible');
+      } else {
+        wrapper.firstElementChild.classList.remove('h3-active');
+        wrapper.lastElementChild.classList.remove('svg-animation');
+        wrapper.nextElementSibling.classList.remove('p-visible');
+      }
+    });
+  });
+});
