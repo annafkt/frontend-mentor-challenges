d4ce405118cbf131fda255618cc330e5fdae7d05
annafkt <44895565+annafkt@users.noreply.github.com>
2024-01-29 16:42:31 +0100

    Add keyboard navigation
    Since I didn't use interactive HTML elements, I had to add keyboard navigation via JavaScript and the tabindex attribute in HTML.

--- faq-accordion-card/script.js
+++ faq-accordion-card/script.js
@@ -1,17 +1,24 @@
 const questionWrappers = document.querySelectorAll('.q-wrapper');
 
 questionWrappers.forEach((wrapper) => {
-  wrapper.addEventListener('click', (e) => {
-    questionWrappers.forEach((wrapper) => {
-      if (wrapper == e.currentTarget) {
-        wrapper.firstElementChild.classList.toggle('h3-active');
-        wrapper.lastElementChild.classList.toggle('svg-animation');
-        wrapper.nextElementSibling.classList.toggle('p-visible');
-      } else {
-        wrapper.firstElementChild.classList.remove('h3-active');
-        wrapper.lastElementChild.classList.remove('svg-animation');
-        wrapper.nextElementSibling.classList.remove('p-visible');
-      }
-    });
+  wrapper.addEventListener('click', showAnswer);
+  wrapper.addEventListener('keydown', (e) => {
+    if (e.keyCode == 13) {
+      showAnswer(e);
+    }
   });
 });
+
+function showAnswer(e) {
+  questionWrappers.forEach((wrapper) => {
+    if (wrapper == e.currentTarget) {
+      wrapper.firstElementChild.classList.toggle('h3-active');
+      wrapper.lastElementChild.classList.toggle('svg-animation');
+      wrapper.nextElementSibling.classList.toggle('p-visible');
+    } else {
+      wrapper.firstElementChild.classList.remove('h3-active');
+      wrapper.lastElementChild.classList.remove('svg-animation');
+      wrapper.nextElementSibling.classList.remove('p-visible');
+    }
+  });
+}
