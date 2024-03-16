d85d1c0b7d54a091e3345c5477068bc0a187d8dc
annafkt <44895565+annafkt@users.noreply.github.com>
2024-03-16 11:46:23 +0100

    Live: move the challenges object to a module file

---
+++ challenge-list.js
@@ -0,0 +1,47 @@
+const challenges = {
+  "newbie": [
+      {
+          "title": "Results summary component",
+          "technologies": "HTML, CSS"
+      },
+      {
+          "title": "Product preview card component",
+          "technologies": "HTML, CSS"
+      },
+      {
+          "title": "Stats preview card component",
+          "technologies": "HTML, CSS"
+      },
+      {
+          "title": "3-column preview card component",
+          "technologies": "HTML, CSS"
+      },
+      {
+          "title": "Social proof section",
+          "technologies": "HTML, SCSS"
+      },
+      {
+          "title": "Article preview component",
+          "technologies": "HTML, SCSS, JS"
+      },
+      {
+          "title": "Interactive rating component",
+          "technologies": "HTML, SCSS, JS"
+      },
+      {
+          "title": "FAQ accordion card",
+          "technologies": "HTML, SCSS, JS"
+      },
+      {
+          "title": "Huddle landing page with single introductory section",
+          "technologies": "HTML, SCSS"
+      },
+      {
+          "title": "Intro component with signup form",
+          "technologies": "HTML, SCSS, JS"
+      }
+  ],
+  "junior": []
+}
+
+export default challenges;
